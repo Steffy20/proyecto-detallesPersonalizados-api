c17d76c47ab908c5404f976cb62c288b207ee76b
Steffy20 <stefanialopez308@gmail.com>
2026-06-10 22:40:36 -0500

    feat: agregar validaciones para pedidos

--- internal/handlers/pedido.go
+++ internal/handlers/pedido.go
@@ -13,6 +13,11 @@ func CrearPedido(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&pedido)
 
+	if pedido.Cliente == "" || pedido.Producto == "" {
+	http.Error(w, "Cliente y producto son obligatorios", http.StatusBadRequest)
+	return
+}
+
 	if err != nil {
 		http.Error(w, "Datos inválidos", http.StatusBadRequest)
 		return
