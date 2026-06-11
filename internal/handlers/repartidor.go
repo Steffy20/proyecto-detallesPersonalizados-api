8f98d046e1ea4579310af1375b9bd5be02c2f7aa
fabrisant <fabripotro02@gmail.com>
2026-06-11 12:12:18 -0500

    Agregar endpoint CrearRepartidor

--- internal/handlers/repartidor.go
+++ internal/handlers/repartidor.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+
+	"proyecto-detalles-api/internal/models"
+	"proyecto-detalles-api/internal/storage"
+)
+
+
+//Crear función CrearRepartidor
+func CrearRepartidor(w http.ResponseWriter, r *http.Request) {
+    var repartidor models.Repartidor
+    json.NewDecoder(r.Body).Decode(&repartidor)
+}
