83599acc57a1b0150061bb4551b1288e29c10152
yandri254 <yairvelez050@gmail.com>
2026-06-11 12:19:46 -0500

    feat: implementar almacenamiento en memoria

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -2,8 +2,11 @@ package storage
 
 import "proyecto-detallesPersonalizados-api/internal/models"
 
-
-//commit: implementar almacenamiento en memoria para pedidos
+// commit: implementar almacenamiento en memoria para pedidos
 var Pedidos = []models.Pedido{}
 var PedidoID = 1
 
+// commit: implementar almacenamiento en memoria para entregas
+var Entregas = []models.Entrega{}
+
+var EntregaID = 1
