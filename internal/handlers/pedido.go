296dcafb9cdf298b9ba4f37dcd21064fb99877c1
Steffy20 <stefanialopez308@gmail.com>
2026-06-10 23:10:11 -0500

    feat: implementar busqueda de pedido por id

--- internal/handlers/pedido.go
+++ internal/handlers/pedido.go
@@ -3,11 +3,15 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 
 	"proyecto-detallesPersonalizados-api/internal/models"
 	"proyecto-detallesPersonalizados-api/internal/storage"
 )
 
+//commit: implementar endpoint crear pedido
 func CrearPedido(w http.ResponseWriter, r *http.Request) {
 
 	var pedido models.Pedido
@@ -19,10 +23,13 @@ func CrearPedido(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+//agregar validaciones para pedidos
 	if pedido.Cliente == "" || pedido.Producto == "" {
 	http.Error(w, "Cliente y producto son obligatorios", http.StatusBadRequest)
 	return
 }
+
+//commit: almacenar pedidos en memoria
 pedido.ID = storage.PedidoID
 storage.PedidoID++
 
@@ -35,9 +42,38 @@ storage.Pedidos = append(storage.Pedidos, pedido)
 }
 
 // GET todos los pedidos
+//commit: implementar listado de pedidos
 func ObtenerPedidos(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(storage.Pedidos)
 }
 
+//GET POR ID
+//commit: implementar busqueda de pedido por id
+
+func ObtenerPedidoPorID(w http.ResponseWriter, r *http.Request) {
+
+	idParam := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest)
+		return
+	}
+
+	for _, pedido := range storage.Pedidos {
+
+		if pedido.ID == id {
+
+			w.Header().Set("Content-Type", "application/json")
+
+			json.NewEncoder(w).Encode(pedido)
+
+			return
+		}
+	}
+
+	http.Error(w, "Pedido no encontrado", http.StatusNotFound)
+}
