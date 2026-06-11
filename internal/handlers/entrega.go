c20f7e11f4ee62c238c9c2c18e358f8103ae3060
yandri254 <yairvelez050@gmail.com>
2026-06-11 14:51:02 -0500

    feat: agregar creación de entrega

--- internal/handlers/entrega.go
+++ internal/handlers/entrega.go
@@ -1,1 +1,31 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"proyecto-detalles-api/internal/models"
+	"proyecto-detalles-api/internal/storage"
+)
+
+func CrearEntrega(w http.ResponseWriter, r *http.Request) {
+
+	var entrega models.Entrega
+
+	err := json.NewDecoder(r.Body).Decode(&entrega)
+
+	if err != nil {
+		http.Error(w, "Datos inválidos", http.StatusBadRequest)
+		return
+	}
+
+	entrega.ID = storage.EntregaID
+	storage.EntregaID++
+
+	storage.Entregas = append(storage.Entregas, entrega)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(entrega)
+}
