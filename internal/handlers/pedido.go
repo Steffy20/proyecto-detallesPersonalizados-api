d17293a90bed8b57e1b4c741bef0ed377d4330cd
Steffy20 <stefanialopez308@gmail.com>
2026-06-10 22:32:33 -0500

    feat: implementar endpoint crear pedido

--- internal/handlers/pedido.go
+++ internal/handlers/pedido.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"proyecto-detallesPersonalizados-api/internal/models"
+)
+
+func CrearPedido(w http.ResponseWriter, r *http.Request) {
+
+	var pedido models.Pedido
+
+	err := json.NewDecoder(r.Body).Decode(&pedido)
+
+	if err != nil {
+		http.Error(w, "Datos inválidos", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(pedido)
+}
