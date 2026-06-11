fb908befcde99cb40090400c9b3dd3aec0c1709f
yandri254 <yairvelez050@gmail.com>
2026-06-11 12:17:51 -0500

    feat: crear modelo Entrega

--- internal/models/entrega.go
+++ internal/models/entrega.go
@@ -1,1 +1,8 @@
 package models
+
+type Entrega struct {
+	ID           int    `json:"id"`
+	Direccion    string `json:"direccion"`
+	Estado       string `json:"estado"`
+	FechaEntrega string `json:"fecha_entrega"`
+}
