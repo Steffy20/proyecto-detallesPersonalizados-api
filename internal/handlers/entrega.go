d724ff6a776be832f678e7bcafc9881ee14da871
yandri254 <yairvelez050@gmail.com>
2026-06-11 12:25:56 -0500

    feat: agregar listado de entregas

--- internal/handlers/entrega.go
+++ internal/handlers/entrega.go
@@ -1,8 +1,38 @@
-package models
+package handlers
 
-type Entrega struct {
-	ID           int    `json:"id"`
-	Direccion    string `json:"direccion"`
-	Estado       string `json:"estado"`
-	FechaEntrega string `json:"fecha_entrega"`
+import (
+	"encoding/json"
+	"net/http"
+
+	"proyecto-detalles-api/internal/models"
+	"proyecto-detalles-api/internal/storage"
+)
+
+func CrearEntrega(w http.ResponseWriter, r *http.Request) {
+
+	var entrega models.Entrega
+
+	err := json.NewDecoder(r.Body).Decode(&entrega)
+
+	if err != nil {
+		http.Error(w, "Datos inválidos", http.StatusBadRequest)
+		return
+	}
+
+	entrega.ID = storage.EntregaID
+	storage.EntregaID++
+
+	storage.Entregas = append(storage.Entregas, entrega)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(entrega)
+}
+
+func ObtenerEntregas(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(storage.Entregas)
 }
