a45649832a95c74c95a1ca00ef92057b011d645f
Steffy20 <stefanialopez308@gmail.com>
2026-06-20 18:46:33 -0500

    refactor: eliminar modulo Repartidor

--- internal/models/repartidor.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Repartidor struct {
-	ID         int    `json:"id"`
-	Nombre     string `json:"nombre"`
-	Telefono   string `json:"telefono"`
-	Disponible bool   `json:"disponible"`
-	Zona       string `json:"zona"`
-}
