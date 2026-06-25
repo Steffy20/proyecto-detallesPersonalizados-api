c23e73169d9ccfc2b33b7f306cd46f7fdf56880b
fabrisanta <fabripotro02@gmail.com>
2026-06-24 23:22:02 -0500

    feat: agregar función ObtenerReclamos

--- internal/handlers/reclamo.go
+++ internal/handlers/reclamo.go
@@ -62,3 +62,8 @@ func CrearReclamo(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(reclamo)
 }
+//obtener todos los reclamos
+func ObtenerReclamos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(storage.Reclamos)
+}
