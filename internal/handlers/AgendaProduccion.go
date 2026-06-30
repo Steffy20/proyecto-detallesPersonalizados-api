509b4a0e7bd472a8029de1d15d52be06ddc9a2c0
yandri254 <yairvelez050@gmail.com>
2026-06-30 17:20:09 -0500

    funcion crear agenda produccion

--- internal/handlers/AgendaProduccion.go
+++ internal/handlers/AgendaProduccion.go
@@ -1,16 +1,16 @@
-package handlers
-
-import (
-	"encoding/json"
-	"net/http"
-	"proyecto-detallesPersonalizados-api/internal/models"
+package handlers 
+import ( 
+"encoding/json" 
+"net/http" 
+"strconv" 
+"github.com/go-chi/chi/v5" 
+"proyecto-detallesPersonalizados-api/internal/models" 
+"proyecto-detallesPersonalizados-api/internal/storage" 
 )
-
-func CrearAgendaProduccion(w http.ResponseWriter, r *http.Request) {
-	var agenda models.AgendaProduccion
-	err := json.NewDecoder(r.Body).Decode(&agenda)
-	if err != nil {
-		http.Error(w, "Datos inválidos", http.StatusBadRequest)
-		return
-	}
-}
+func CrearAgendaProduccion(w http.ResponseWriter, r *http.Request) { 
+var agenda models.AgendaProduccion 
+err := json.NewDecoder(r.Body).Decode(&agenda) 
+if err != nil { 
+http.Error(w, "Datos inválidos", http.StatusBadRequest) 
+return 
+} 
