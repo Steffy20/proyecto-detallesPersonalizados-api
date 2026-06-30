9b2a255b21aff470da0bf1608b99266bab6975ff
yandri254 <yairvelez050@gmail.com>
2026-06-30 17:38:10 -0500

    guardar en memoria de slot produccion

--- internal/handlers/SlotProduccion.go
+++ internal/handlers/SlotProduccion.go
@@ -1,30 +1,41 @@
-package handlers 
-import ( 
-"encoding/json" 
-"net/http" 
-"strconv" 
-"github.com/go-chi/chi/v5" 
-"proyecto-detallesPersonalizados-api/internal/models" 
-"proyecto-detallesPersonalizados-api/internal/storage" 
-) 
-func CrearSlotProduccion(w http.ResponseWriter, r *http.Request) { 
-var slot models.SlotProduccion 
-err := json.NewDecoder(r.Body).Decode(&slot) 
-if err != nil { 
-http.Error(w, "Datos inválidos", http.StatusBadRequest) 
-  return 
- } 
- // Validar que la agenda exista 
- agendaExiste := false 
- 
- for _, agenda := range storage.AgendasProduccion { 
-  if agenda.ID == slot.AgendaID { 
-   agendaExiste = true 
-   break 
-  } 
- } 
-if !agendaExiste { 
-http.Error(w, "La agenda asociada no existe", 
-http.StatusBadRequest) 
-return 
-} 
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"proyecto-detallesPersonalizados-api/internal/models"
+	"proyecto-detallesPersonalizados-api/internal/storage"
+)
+
+func CrearSlotProduccion(w http.ResponseWriter, r *http.Request) {
+	var slot models.SlotProduccion
+	err := json.NewDecoder(r.Body).Decode(&slot)
+	if err != nil {
+		http.Error(w, "Datos inválidos", http.StatusBadRequest)
+		return
+	}
+	// Validar que la agenda exista
+	agendaExiste := false
+
+	for _, agenda := range storage.AgendasProduccion {
+		if agenda.ID == slot.AgendaID {
+			agendaExiste = true
+			break
+		}
+	}
+	if !agendaExiste {
+		http.Error(w, "La agenda asociada no existe",
+			http.StatusBadRequest)
+		return
+	}
+	// Guardar en memoria
+	slot.ID = storage.SlotProduccionID
+	storage.SlotProduccionID++
+	storage.SlotsProduccion = append(
+		storage.SlotsProduccion,
+		slot,
+	)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(slot)
+}
