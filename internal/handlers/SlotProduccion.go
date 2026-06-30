b881f5abf396841b9fb011b0dd73f8423b57e749
yandri254 <yairvelez050@gmail.com>
2026-06-30 17:41:26 -0500

    funcion actualizar slotsproduccion

--- internal/handlers/SlotProduccion.go
+++ internal/handlers/SlotProduccion.go
@@ -62,3 +62,16 @@ func ObtenerSlotProduccionPorID(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Error(w, "Slot de producción no encontrado", http.StatusNotFound)
 }
+func ActualizarSlotProduccion(w http.ResponseWriter, r *http.Request) { 
+idParam := chi.URLParam(r, "id") 
+id, err := strconv.Atoi(idParam) 
+if err != nil { 
+http.Error(w, "ID inválido", http.StatusBadRequest) 
+return 
+} 
+var slotActualizado models.SlotProduccion 
+err = json.NewDecoder(r.Body).Decode(&slotActualizado) 
+if err != nil { 
+http.Error(w, "Datos inválidos", http.StatusBadRequest) 
+return 
+} 
